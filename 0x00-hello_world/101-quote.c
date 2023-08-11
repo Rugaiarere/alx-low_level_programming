8b1491baf58f0002f633baefd1c709ffc1614c55
Rugaiarere <rugaiashahimgmail.com>
2023-08-11 09:11:21 -0700

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,9 @@
 * main - Entry point
 *
 * Description: prints a qoute using  with write function
-	 *write(int fd.const void *buf.size_t count)
+	 ssize_t write(int fd.const void *buf.size_t count);
 
-* Return: Always 1 (not Success)
+* Return: Always 1 (not Success).
 */
 
 int main(void)
