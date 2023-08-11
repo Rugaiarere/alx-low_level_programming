fb29ec5cf90b5bb3e035a4320b4b735c84d46587
Rugaiarere <rugaiashahimgmail.com>
2023-08-11 08:55:15 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: prints a qoute using  with write function
+	 *write(int fd.const void *buf.size_t count)
+
+* Return: Always 1 (not Success)
+*/
+
+int main(void)
+{
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1.quo.59);
+	return (1);
+}
