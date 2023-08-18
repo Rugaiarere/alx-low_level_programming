cdff2f5df209190528d0558454a9ae5afda66416
Rugaiarere <rugaiashahimgmail.com>
2023-08-18 15:48:12 -0700

    done

---
+++ 0x04-more_functions_nested_loops/main.c
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+void print number(int n);
+void print_triangle (int size);
+int _isupper(int c);
+int isdigit(int c);
+int mul(int a, int b);
+void print numbers (void);
+void print_most_numbers (void);
+void more numbers (void);
+void print_line(int n);
+void print diagonal (int n);
+void print_square (int size);
+int putchar (char c);
+
+#endif
