04783d1f80fb60995740bcba1f1eb648dd570158
Rugaiarere <rugaiashahimgmail.com>
2023-08-24 13:28:16 -0700

    done

--- 0x06-pointers_arrays_strings/_putchar.c
+++ 0x06-pointers_arrays_strings/_putchar.c
@@ -1,21 +1,13 @@
 #include <unistd.h>
-
 #include "main.h"
-
 /**
-
-*putchar writes the character c to stdout Oc: The character to print
-
-*Return: On success 1.
-
-*
-
-*on error, -1 is returned, and errno is set appropriately.
-
-*/
-
-int _putchar (char c)
-
-r
-
-return (write(1, &c. 1));
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
